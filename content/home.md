36db11ec2a3fac6deee4007e5a80e7922e7c1d92
Ryan Benkeser <rbenkeser@thousandeyes.com>
2020-04-25 10:51:40 -0700

    Add all my files

---
+++ content/home.md
@@ -0,0 +1,11 @@
+Title: Day 38
+Date: 2020-04-24 11:27PM PST
+Author: Ryan Benkeser
+
+## Day 38 of Quarantine
+
+It has been 38 days since the infamous Coronavirus lockdown started.  My name is Ryan Benkeser and I am starting the Data Deductions blog today.  Why am I doing this, you ask?
+
+Great question! The answer is very complex and multifaceted.  I am bored.
+
+The purpose of this blog will be to share cool data trends across a wide array of subjects.  It will cross the chasms of Economics, Finance, Sports, Politics, the Environment and everything in between.  Stay tuned!
---
+++ pelicanconf.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = 'Ryan Benkeser'
+SITENAME = 'Data Deductions'
+SITETITLE = 'Data Deductions'
+SITESUBTITLE = 'By Ryan Benkeser, Data Scientist'
+SITEDESCRIPTION = 'Making deductions from data'
+
+PATH = 'content'
+
+TIMEZONE = 'America/Los_Angeles'
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('About Me', 'http://linkedin.com/in/ryanbenkeser/'),
+         ('Articles', 'http://github.com/ryanbenkeser/'))
+
+# Social widget
+SOCIAL = (('linkedin','http://linkedin.com/in/ryanbenkeser/'),
+          ('github', 'http://github.com/ryanbenkeser/'),)
+
+DEFAULT_PAGINATION = 5
+
+THEME = "/Users/rbenkeser/opt/anaconda3/lib/python3.7/site-packages/pelican/themes/Flex"
+
+
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
